cec687304df550aafca1d3fc29921e647680c2c3
vihaniscool <75142511+vihaniscool@users.noreply.github.com>
2023-08-20 05:03:14 +1200

    Add files via upload

---
+++ Walkers.py
@@ -0,0 +1,32 @@
+import cv2
+
+
+# Create our body classifier
+body_classifier = cv2.CascadeClassifier('C:/Users/VihanPC/Downloads/PRO-106-ProjectTemplate-main/PRO-106-ProjectTemplate-main/haarcascade_fullbody.xml')
+
+# Initiate video capture for video file
+cap = cv2.VideoCapture('C:/Users/VihanPC/Downloads/PRO-106-ProjectTemplate-main/PRO-106-ProjectTemplate-main/walking.avi')
+
+# Loop once video is successfully loaded
+while True:
+    
+    # Read first frame
+    ret, frame = cap.read()
+
+    #Convert Each Frame into Grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Pass frame to our body classifier
+    bodies = body_classifier.detectMultiScale(gray, 1.2, 3)
+    
+    # Extract bounding boxes for any bodies identified
+    for (x,y,w,h) in bodies:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+    
+    cv2.imshow('frame',frame)
+
+    if cv2.waitKey(1) == 32: #32 is the Space Key
+        break
+
+cap.release()
+cv2.destroyAllWindows()
